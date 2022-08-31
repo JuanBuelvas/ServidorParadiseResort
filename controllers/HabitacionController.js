36b38b8a44064b543ea66afbfd091f099f4d0016
201 <201@academicos.cesde.edu.co>
2022-08-30 20:23:33 -0500

    se realiza prueba

--- controllers/HabitacionController.js
+++ controllers/HabitacionController.js
@@ -31,12 +31,12 @@ export class HabitacionController{
         //Instanciar HabitacionService
         let habitacionService = new HabitacionService();
         
-        let id = request.params.id;
+        let ide = request.params.id;
 
         try{
             response.status(200).json({
-                mensaje:"Exito en la consulta " + id,
-                datos: await habitacionService.buscarPorId(id)
+                mensaje:"Exito en la consulta " + ide,
+                datos: await habitacionService.buscarPorId(ide)
             })
             response.json(datos);
         }catch(error){ //Fallo Resolviendo la Peticion
